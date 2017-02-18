aaff11b64af623fe9913474eb2290a8ca4f72abe
Maden23 <maden98@gmail.com>
2017-02-18 16:37:49 +0300

    Client -> 'Orders'
    Creating new records in 'Orders' table using UI

--- Taxi23CenturyFoxClient/Taxi23CenturyFoxClient.lpr
+++ Taxi23CenturyFoxClient/Taxi23CenturyFoxClient.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, Main
+  Forms, Main, lazcontrols
   { you can add units after this };
 
 {$R *.res}
--- Taxi23CenturyFoxClient/main.pas
+++ Taxi23CenturyFoxClient/main.pas
@@ -5,10 +5,45 @@ unit Main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, sqldb, sqlite3conn, FileUtil, Forms,
+  Controls, Graphics, Dialogs, StdCtrls, DbCtrls, ComCtrls, Spin,
+  ExtCtrls, EditBtn;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    TestQuery: TSQLQuery;
+    WideTrunk: TCheckBox;
+    BabySeat: TCheckBox;
+    CheckGroup1: TCheckGroup;
+    EditStart: TEdit;
+    EditFinish: TEdit;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Hours: TSpinEdit;
+    Minutes: TSpinEdit;
+    SQLite3Connection1: TSQLite3Connection;
+    OrderQuery: TSQLQuery;
+    AdressQuery: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    ComfortRate: TTrackBar;
+    Passengers: TTrackBar;
+    procedure Button1Click(Sender: TObject);
+    procedure ComfortRateChange(Sender: TObject);
+    procedure PassengersChange(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +56,107 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+function CheckAdress (adress: string; AdressQuery: TSQLQuery): string;
+begin
+  result := '0';
+  AdressQuery.Close;
+  AdressQuery.ParamByName('adress').AsString := adress; //парам - туда
+  AdressQuery.Open;
+  result :=  IntToStr(AdressQuery.FieldByName('id').Asinteger); //филд - обратна
+
+end;
+
+function ConvertToMinutes (hours: integer; minutes: integer): integer;
+begin
+  result := hours*60 + minutes;
+end;
+
+function BoolToInt (Bool: boolean): integer;
+begin
+  if Bool = false then
+     result := 0
+  else
+     result := 1;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var str1, str2 : string;
+    time: integer;
+begin
+  try
+     SQLite3Connection1.Open;
+     SQLTransaction1.Active := True;
+  except
+     ShowMessage ('Ошибка подключения к базе данных!');
+  end;
+
+  str1 := CheckAdress(EditStart.Text, AdressQuery);
+  str2 := CheckAdress(EditFinish.Text, AdressQuery);
+  if (str1 = '0') then
+     ShowMessage('Start adress: "' + EditStart.Text + '" does not exist');
+  if (str2 = '0') then
+     ShowMessage('Finish adress: "' + EditFinish.Text + '" does not exist')
+  else
+     begin
+        Time := ConvertToMinutes(Hours.Value, Minutes.Value);
+        with OrderQuery do
+             begin
+                  Open;
+                  Insert;
+                  Fields[1].AsInteger := StrToInt(str1);
+                  Fields[2].AsInteger := StrToInt(str2);
+                  Fields[3].AsDatetime := Now;
+                  Fields[4].AsInteger := ComfortRate.Position;
+                  Fields[5].AsInteger := Passengers.Position;
+                  Fields[6].AsInteger := BoolToInt(WideTrunk.Checked);
+                  Fields[7].AsInteger := BoolToInt(BabySeat.Checked);
+                  Fields[8].AsInteger := Time;
+                  try
+
+                     Post;
+                     ApplyUpdates;
+                     SQLTransaction1.Commit;
+                  except ShowMessage('Запрос не выполнен!');
+                  end;
+             end;
+
+         {ShowMessage(str1 +', '+str2+', '+datetimetostr(now)+', '+inttostr(ComfortRate.Position)
+                          +', '+inttostr(Passengers.Position)+', '+inttostr(BoolToInt(WideTrunk.Checked))
+                          +', '+inttostr(BoolToInt(BabySeat.Checked))+', '+inttostr(Time)); }
+     end;
+     ShowMessage('Заказ принят');
+end;
+
+
+procedure TForm1.ComfortRateChange(Sender: TObject);
+begin
+  case ComfortRate.Position of
+       1: Label5.Caption := '✪';
+       2: Label5.Caption := '✪✪';
+       3: Label5.Caption := '✪✪✪';
+       4: Label5.Caption := '✪✪✪✪';
+       5: Label5.Caption := '✪✪✪✪✪';
+
+  end;
+end;
+
+
+procedure TForm1.PassengersChange(Sender: TObject);
+begin
+  case Passengers.Position of
+       1: Label7.Caption := '❶';
+       2: Label7.Caption := '❷';
+       3: Label7.Caption := '❸';
+       4: Label7.Caption := '❹';
+       5: Label7.Caption := '❺';
+       6: Label7.Caption := '❻';
+       7: Label7.Caption := '❼';
+       8: Label7.Caption := '❽';
+
+  end;
+end;
 
 end.
 
